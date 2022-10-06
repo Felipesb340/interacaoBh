5e505976e401cc8726a9d5f2c2dce5d82ec119bc
Felipe Soares <felipesb340@hotmail.com>
2022-10-06 17:17:17 -0300

    3 commit

--- src/pages/profissionais/index.tsx
+++ src/pages/profissionais/index.tsx
@@ -3,18 +3,6 @@ import styles from './styles.module.scss';
 import Modal from 'react-modal';
 import { useState } from 'react';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
-
 export default function Profissionais() {
 
 
